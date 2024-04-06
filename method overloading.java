e1972816902c0cadffc47c5b2e71f88faf9e7826
sadaf45 <sadafishaq777@gmail.com>
2024-04-06 12:24:33 +0530

    hiiiiiii

---
+++ method overloading.java
@@ -0,0 +1,17 @@
+class Add
+{
+public void add(int a,int b)
+{
+System.out.println("Addition of two numbers is"+(a+b));
+}
+void add(int a,int b,int c)
+{
+System.out.println("expression is"+(a+b-c));
+}
+public static void main(String args[])
+{
+Add a1=new Add();
+a1.add(3,6);
+a1.add(3,5,6);
+}
+}
